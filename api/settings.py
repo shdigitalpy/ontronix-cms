b168ee6e766b5ca4d832fb6bd63327a7c57ba6bd
Sandro Huber <sandro@sh-digital.ch>
2023-12-21 15:55:32 +0100

    update middleware

--- api/settings.py
+++ api/settings.py
@@ -79,15 +79,16 @@ SITE_ID = 1
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_brotli.middleware.BrotliMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
